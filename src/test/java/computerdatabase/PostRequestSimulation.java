1af54923ae561d98c35f21fd71cfc42615f5a2b6
vkorogoda <viktar.karahoda@ontravelsolutions.com>
2025-01-29 01:37:16 +0100

    fix concurrency

--- src/test/java/computerdatabase/PostRequestSimulation.java
+++ src/test/java/computerdatabase/PostRequestSimulation.java
@@ -1,6 +1,7 @@
 package computerdatabase;
 
 import static io.gatling.javaapi.core.CoreDsl.StringBody;
+import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
 import static io.gatling.javaapi.core.CoreDsl.exec;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
@@ -24,22 +25,42 @@ public class PostRequestSimulation extends Simulation {
     // Define the JSON body for the POST request
     private final String requestBody = "{\n"
             + "  \"id\": 1,\n"
-            + "  \"time\" : 100,\n"
+            + "  \"time\" : 123,\n"
             + "  \"name\" : \"one\"\n"
             + "}"; // Replace with your JSON body
 
-    // Define the scenario
+    // 50req/sec during 60sec
     private final ScenarioBuilder scn = scenario("PostRequestSimulation")
-            .repeat(200)
-            .on(exec(
+            .exec(
                         http("Send POST Request")
                                 .post("/accept") // Replace with your endpoint
                                 .body(StringBody(requestBody)) // Attach the JSON body
                                 .check(status().is(200)) // Validate the response status
 
                 )
-            )
             .pause(Duration.ofMillis(20), Duration.ofMillis(100)); // 20-100ms pause between requests
+
+    {
+        setUp(
+                scn.injectOpen(
+                        constantUsersPerSec(50).during(60) // 5 threads
+                )
+        ).protocols(httpProtocol);
+    }
+
+    // 200 times every 20-200ms with 5 requests in one time
+/*    private final ScenarioBuilder scn = scenario("PostRequestSimulation")
+            .repeat(200) // Each user sends 200 requests (1000 total requests / 5 users)
+            .on(
+                    exec(
+                            http("Send POST Request")
+                                    .post("/accept") // Replace with your endpoint
+                                    .body(StringBody(requestBody)) // Attach the JSON body
+                                    .check(status().is(200)) // Validate the response status
+
+                    )
+            )
+            .pause(Duration.ofMillis(20), Duration.ofMillis(200)); // random 20-100ms pause between requests
 
     {
         setUp(
@@ -47,5 +68,5 @@ public class PostRequestSimulation extends Simulation {
                         atOnceUsers(5) // 5 threads
                 )
         ).protocols(httpProtocol);
-    }
+    }*/
 }
